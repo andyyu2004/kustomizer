353d9e60a9e1f4a1473e8959ba5b683d99101007
Andy Yu <andyyu2004@gmail.com>
2025-08-05 22:38:55 +1200

    implement include selectors and include templates

--- kustomizer/src/transform/label.rs
+++ kustomizer/src/transform/label.rs
@@ -12,27 +12,28 @@ impl Transformer for LabelTransformer<'_> {
 
         let builtins = fieldspec::Builtin::get();
 
-        // FIXME double check definition of these kustomizer/src/fieldspec/metadataLabels.yaml
-        // seems to overshoot what reference impl does
-        // let field_specs = if label.include_selectors {
-        //     builtins.common_labels
-        // } else {
-        //     builtins.metadata_labels
-        // };
-        let field_specs = &builtins.metadata_labels;
+        for label in self.0 {
+            if label.pairs.is_empty() {
+                continue;
+            }
 
-        for resource in resources.iter_mut() {
-            for field_spec in field_specs.iter() {
-                field_spec.apply(resource, |labels| {
-                    for label in self.0 {
+            let field_specs = match (label.include_selectors, label.include_templates) {
+                (true, _) => &builtins.common_labels,
+                (false, true) => &builtins.template_labels,
+                (false, false) => &builtins.metadata_labels,
+            };
+
+            for resource in resources.iter_mut() {
+                for field_spec in field_specs.iter() {
+                    field_spec.apply(resource, |l| {
                         for (key, value) in &label.pairs {
-                            labels.insert(
+                            l.insert(
                                 serde_yaml::Value::String(key.to_string()),
                                 serde_yaml::Value::String(value.to_string()),
                             );
                         }
-                    }
-                });
+                    });
+                }
             }
         }
     }
