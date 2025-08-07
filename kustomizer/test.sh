e55620d74f8ff66561fe2a2a77a684e97cd255a7
Andy Yu <andyyu2004@gmail.com>
2025-08-07 22:44:35 +1200

    tests almost passing now

--- kustomizer/test.sh
+++ kustomizer/test.sh
@@ -3,10 +3,8 @@ set -euo pipefail
 a=$(mktemp)
 b=$(mktemp)
 
-cargo run -- build ../../../partly-argocd/shared/resources/partly >"$a" &
-
-kustomize --load-restrictor LoadRestrictionsNone --enable-alpha-plugins --enable-exec build ../../../partly-argocd/shared/resources/partly >"$b" &
-
+kustomize --load-restrictor LoadRestrictionsNone --enable-alpha-plugins --enable-exec build ../../../partly-argocd/shared/resources/partly >"$a" &
+cargo run -- build ../../../partly-argocd/shared/resources/partly >"$b"
 wait
 
 dyff between --color=on --ignore-order-changes --ignore-whitespace-changes --set-exit-code "$a" "$b"
