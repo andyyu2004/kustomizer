6da2e4425d3f53891399677efdac0c82e6d2547f
Andy Yu <andyyu2004@gmail.com>
2025-08-31 13:52:14 +1200

    don't support windows in CI

--- flake.nix
+++ flake.nix
@@ -10,10 +10,36 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        kustomizer = pkgs.rustPlatform.buildRustPackage {
+          pname = "kustomizer";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          buildInputs = [ ];
+          nativeBuildInputs = [ ];
+
+          meta = with pkgs.lib; {
+            description = "Faster rust port of kustomize";
+            homepage = "https://github.com/andyyu2004/kustomizer";
+            license = licenses.mit;
+          };
+        };
       in
       {
+        packages = {
+          default = kustomizer;
+          kustomizer = kustomizer;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            # Test dependencies
             kustomize
             kustomize-sops
             sops
@@ -21,7 +47,7 @@
           ];
 
           shellHook = ''
-            # kustomize-sops doesn't expose ksops binary by default for some reason
+            # kustomize-sops installs ksops binary at a custom path
             export PATH=${pkgs.kustomize-sops}/lib/viaduct.ai/v1/ksops:$PATH
           '';
         };
