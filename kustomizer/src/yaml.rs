ee6ad36ca05d8014f9b749c11bdd7f54f10d13d4
Andy Yu <andyyu2004@gmail.com>
2026-01-29 15:41:55 +1300

    at least this quotes yes and no, but generates weird multilines

--- kustomizer/src/yaml.rs
+++ kustomizer/src/yaml.rs
@@ -22,16 +22,26 @@ where
     serde_saphyr::from_reader(reader).map_err(Into::into)
 }
 
+const OPTS: serde_saphyr::SerializerOptions = serde_saphyr::SerializerOptions {
+    prefer_block_scalars: true,
+    empty_as_braces: true,
+    indent_step: 2,
+    anchor_generator: None,
+    min_fold_chars: 32,
+    folded_wrap_chars: 80,
+    tagged_enums: false,
+};
+
 pub fn to_string<T>(value: &T) -> anyhow::Result<String>
 where
     T: serde::Serialize,
 {
-    serde_saphyr::to_string(value).map_err(Into::into)
+    serde_saphyr::to_string_with_options(value, OPTS).map_err(Into::into)
 }
 
 pub fn to_fmt_writer<W: std::fmt::Write, T: serde::Serialize>(
     output: &mut W,
     value: &T,
 ) -> anyhow::Result<()> {
-    serde_saphyr::to_fmt_writer(output, value).map_err(Into::into)
+    serde_saphyr::to_fmt_writer_with_options(output, value, OPTS).map_err(Into::into)
 }
