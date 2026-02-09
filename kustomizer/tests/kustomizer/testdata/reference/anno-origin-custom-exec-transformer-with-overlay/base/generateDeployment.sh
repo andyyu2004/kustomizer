13831e694fb10707748dd4b3a2971b60f5f1d48b
Andy Yu <andyyu2004@gmail.com>
2026-02-09 17:49:43 +1300

    Bug repro: buildMetadata not supported with exec plugins in overlay
    Test: anno-origin-custom-exec-transformer-with-overlay
    Source: kustomize/api/krusty/transformerannotation_test.go:TestAnnoOriginCustomExecTransformerWithOverlay

---
+++ kustomizer/tests/kustomizer/testdata/reference/anno-origin-custom-exec-transformer-with-overlay/base/generateDeployment.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cat <<EEOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx
+  labels:
+    app: nginx
+  annotations:
+    tshirt-size: small # this injects the resource reservations
+    config.kubernetes.io/origin: |
+      path: somefile.yaml
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx
+EEOF
